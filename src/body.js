b25beff4bef0b1224fd81486fa317fd48cf3c2d8
Ryan <ryandodd0115@gmail.com>
2023-09-27 20:57:04 -0500

    Main.css adjustments, navbar nearly finalized. Additional head enhancements added

---
+++ src/body.js
@@ -0,0 +1,4 @@
+// This contains code for the body of the page
+function about() {
+    document.getElementById("main").innerHTML = null; // Will populate later. Need to build out a look for this.
+}

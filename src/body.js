51f8cc21904246bcf3afb5ad055a57d33f8d7c00
Ryan <ryandodd0115@gmail.com>
2023-09-28 12:44:41 -0500

    Updated resume, new body.js content, and all clickable tabs work. Links refined

--- src/body.js
+++ src/body.js
@@ -1,7 +1,55 @@
 // This contains code for the body of the page
+let currentPortfolioImg = 1;
+let currentExperiencePage = 1;
+const TOTAL_PORTFOLIO_IMAGES = 12;
+const TOTAL_EXPERIENCE_PAGES = 2;
 
 function about() {
-    fetch('./pages/about.html')
-        .then(response=> response.text())
-        .then(text=> document.getElementById('main').innerHTML = text);
+    document.getElementById('about').style.display = "flex";
+    document.getElementById('experience').style.display = "none";
+    document.getElementById('portfolio').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+}
+
+function experience() {
+    document.getElementById('about').style.display = "none";
+    document.getElementById('experience').style.display = "flex";
+    document.getElementById('portfolio').style.display = "none";
+    document.getElementById('contact').style.display = "none";
+}
+
+function portfolio() {
+    document.getElementById('about').style.display = "none";
+    document.getElementById('experience').style.display = "none";
+    document.getElementById('portfolio').style.display = "flex";
+    document.getElementById('contact').style.display = "none";
+}
+
+function contact() {
+    document.getElementById('about').style.display = "none";
+    document.getElementById('experience').style.display = "none";
+    document.getElementById('portfolio').style.display = "none";
+    document.getElementById('contact').style.display = "flex";
+}
+
+function loadPortfolio() {
+
+}
+
+function portfolioForward() {
+    if (currentPortfolioImg + 1 > TOTAL_PORTFOLIO_IMAGES) {
+        currentPortfolioImg = 1;
+    } else {
+        currentPortfolioImg++;
+    }
+    loadPortfolio();
+}
+
+function portfolioBackward() {
+    if (currentPortfolioImg - 1 <= 0) {
+        currentPortfolioImg = TOTAL_PORTFOLIO_IMAGES;
+    } else {
+        currentPortfolioImg--;
+    }
+    loadPortfolio();
 }

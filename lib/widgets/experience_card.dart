dc7b04f0f8caf9a7fcc3853fcae6c3497a876e01
Ryan <ryandodd0115@gmail.com>
2024-05-26 18:41:08 -0500

    mobile version of experience

--- lib/widgets/experience_card.dart
+++ lib/widgets/experience_card.dart
@@ -23,96 +23,93 @@ class _ExperienceCardState extends State<ExperienceCard> {
     ThemeData theme = Theme.of(context);
     MediaQueryData queryData = MediaQuery.of(context);
 
-    return SizedBox(
-      height: queryData.size.width * 0.07,
-      width: queryData.size.width * 0.41,
-      child: MouseRegion(
-        onEnter: (event) {
-          setState(() {
-            isHovered = true;
-          });
-        },
-        onExit: (event) {
-          setState(() {
-            isHovered = false;
-          });
-        },
-        child: GestureDetector(
-          onTap: () => showDialog<String>(
-            context: context,
-            builder: (BuildContext context) => AlertDialog(
-              backgroundColor: theme.colorScheme.inversePrimary,
-              icon: SizedBox(
-                height: queryData.size.width * 0.07,
-                width: queryData.size.width * 0.10,
-                child: Image.asset(widget.companyLogo)
-              ),
-              title: Text("${widget.company} | ${widget.title}"),
-              content: Text("${widget.dates}\n${widget.description}",
-                softWrap: true,
-                overflow: TextOverflow.clip
-              ),
-              actions: <Widget>[
-                TextButton(
-                  onPressed: () => Navigator.pop(context, 'OK'),
-                  child: const Text('Close'),
-                ),
-              ],
+    return MouseRegion(
+      onEnter: (event) {
+        setState(() {
+          isHovered = true;
+        });
+      },
+      onExit: (event) {
+        setState(() {
+          isHovered = false;
+        });
+      },
+      child: GestureDetector(
+        onTap: () => showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            backgroundColor: theme.colorScheme.inversePrimary,
+            icon: SizedBox(
+              height: queryData.size.width * 0.07,
+              width: queryData.size.width * 0.10,
+              child: Image.asset(widget.companyLogo)
             ),
-          ),
-          child: Card(
-            elevation: isHovered ? 20 : 12,
-            color: isHovered ? theme.colorScheme.primary : theme.colorScheme.onSurface,
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: SizedBox(
-                      height: queryData.size.width * 0.07,
-                      width: queryData.size.width * 0.10,
-                      child: Image.asset(widget.companyLogo)
-                    ),
-                  ),
-                  Flexible(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.start,
-                      children: [
-                        Text(widget.title, 
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: isHovered ? theme.colorScheme.onSecondary : theme.colorScheme.primary
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                        Text(widget.description, 
-                          style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            color: isHovered ? theme.colorScheme.onSecondary : theme.colorScheme.primary
-                          ),
-                          softWrap: true,
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.left,
-                        ),
-                      ]
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: IconButton(
-                      icon: const Icon(Icons.link_sharp),
-                      onPressed: () {
-                        launchUrl(Uri.parse(widget.link),
-                        mode: LaunchMode.externalApplication);
-                      }
-                    ),
-                  ),
-                ]
+            title: Text("${widget.company} | ${widget.title}"),
+            content: Text("${widget.dates}\n${widget.description}",
+              softWrap: true,
+              overflow: TextOverflow.clip
+            ),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'OK'),
+                child: const Text('Close'),
               ),
-            )
+            ],
           ),
+        ),
+        child: Card(
+          clipBehavior: Clip.hardEdge,
+          elevation: isHovered ? 20 : 12,
+          color: isHovered ? theme.colorScheme.primary : theme.colorScheme.onSurface,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: queryData.size.width * 0.07,
+                    width: queryData.size.width * 0.10,
+                    child: Image.asset(widget.companyLogo)
+                  ),
+                ),
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(widget.title, 
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: isHovered ? theme.colorScheme.onSecondary : theme.colorScheme.primary
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Text(widget.description, 
+                        style: TextStyle(
+                          fontWeight: FontWeight.normal,
+                          color: isHovered ? theme.colorScheme.onSecondary : theme.colorScheme.primary
+                        ),
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        textAlign: TextAlign.left,
+                      ),
+                    ]
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: IconButton(
+                    icon: const Icon(Icons.link_sharp),
+                    onPressed: () {
+                      launchUrl(Uri.parse(widget.link),
+                      mode: LaunchMode.externalApplication);
+                    }
+                  ),
+                ),
+              ]
+            ),
+          )
         ),
       ),
     );

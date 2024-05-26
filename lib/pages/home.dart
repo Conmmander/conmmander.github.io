7614b79b60a4e8e8593f6d0cad1ffdebc9f03c87
Ryan <ryandodd0115@gmail.com>
2024-05-26 18:22:46 -0500

    mobile version of home

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -7,53 +7,95 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     ThemeData theme = Theme.of(context);
     MediaQueryData queryData = MediaQuery.of(context);
+    bool mobilePlatform = queryData.size.width >= 600 ? false : true;
 
-    return Scaffold(
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(100.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      boxShadow: [
-                        BoxShadow(
-                          color: theme.colorScheme.shadow,
-                          blurRadius: 4.0,
-                          spreadRadius: 1.0,
-                          offset: const Offset(0, 0)
-                        )
-                      ],
-                      shape: BoxShape.circle
+    if (!mobilePlatform) {
+      return Scaffold(
+        body: Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(100.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: theme.colorScheme.shadow,
+                            blurRadius: 4.0,
+                            spreadRadius: 1.0,
+                            offset: const Offset(0, 0)
+                          )
+                        ],
+                        shape: BoxShape.circle
+                      ),
+                      clipBehavior: Clip.hardEdge,
+                      child: Image.asset('assets/img/personal/me.jpeg',
+                        width: queryData.size.width * 0.20,
+                        height: queryData.size.width * 0.20,
+                        fit: BoxFit.cover
+                      ),
                     ),
-                    clipBehavior: Clip.hardEdge,
-                    child: Image.asset('assets/img/personal/me.jpeg',
-                      width: queryData.size.width * 0.20,
-                      height: queryData.size.width * 0.20,
-                      fit: BoxFit.cover
-                    ),
-                  ),
-                )
-              ]
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text("Programmer\nTech Enthusiast",
-                  style: TextStyle(
-                    fontSize: queryData.size.width *0.026,
-                    color: theme.colorScheme.onPrimary
                   )
-                ),
-              ]
-            ),
-          ],
+                ]
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Programmer\nTech Enthusiast",
+                    style: TextStyle(
+                      fontSize: queryData.size.width *0.026,
+                      color: theme.colorScheme.onPrimary
+                    )
+                  ),
+                ]
+              ),
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    } else {
+      return Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("Programmer\nTech Enthusiast",
+              textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: queryData.size.width *0.03,
+                  color: theme.colorScheme.onPrimary
+                )
+              ),
+              Padding(
+                padding: const EdgeInsets.all(30.0),
+                child: Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: theme.colorScheme.shadow,
+                        blurRadius: 4.0,
+                        spreadRadius: 1.0,
+                        offset: const Offset(0, 0)
+                      )
+                    ],
+                    shape: BoxShape.circle
+                  ),
+                  clipBehavior: Clip.hardEdge,
+                  child: Image.asset('assets/img/personal/me.jpeg',
+                    width: queryData.size.width * 0.40,
+                    height: queryData.size.width * 0.40,
+                    fit: BoxFit.cover
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
   }
 }
